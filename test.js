1b529beeb170a6ba01f53625b3a750a689925dca
Ashton Meuser <ashtonmeuser@gmail.com>
2019-10-28 10:32:37 -0700

    DynamoDB table resource, create endpoint
    Added CloudFormation DynamoDB table resource
    Create feeding record with UUID, dummy info

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
-module.exports.handler = async () => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: 'test',
-  }),
-});
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const tableName = 'freshworks-demo-feeding-table';
+
+module.exports.handler = async () => {
+  try {
+    const feeding = {
+      id: uuid(),
+      location: 'Victoria',
+      time: '12:34',
+      foodType: 'bread',
+      foodQuantity: 2,
+      duckQuantity: 3,
+    };
+    const params = {
+      TableName: tableName,
+      Item: feeding,
+      ReturnValues: 'NONE',
+    };
+    await dynamoDb.put(params).promise();
+    return {
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
